ac825800b9c41f662f679454900c8bb63450ce83
Gustavo <guatarashi@gmail.com>
2023-07-25 22:05:10 -0300

    feat: Testando o distinct.

--- src/br/com/atarashi/main.kt
+++ src/br/com/atarashi/main.kt
@@ -1,40 +1,9 @@
 package src.br.com.atarashi
 
 fun main() {
-    val banco = BancoDeNomes()
-    val nomesSalvos: Collection<String> = banco.nomes
+    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
 
-    banco.salva("Alex")
-    println(nomesSalvos)
-    println(BancoDeNomes().nomes)
-}
-
-class BancoDeNomes {
-    val nomes: Collection<String> get() = dados.toList()
-
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
-
-    companion object {
-        private val dados = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val nomes: List<String> = listOf(
-        "Alex",
-        "Fran",
-        "Gui",
-        "Maria",
-        "Ana"
-    )
-
-    for (nome in nomes) {
-        println(nome)
-    }
-
-    println(nomes)
-    println("Tem o nome Alex? ${nomes.contains("Alex")}")
-    println("Tamanho da coleção ${nomes.size}")
+    println(assistiramAmbos.distinct())
 }
---
+++ src/br/com/atarashi/testaCollection.kt
@@ -0,0 +1,41 @@
+package src.br.com.atarashi
+
+
+fun testaCopia() {
+    val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
+
+    banco.salva("Alex")
+    println(nomesSalvos)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+    val nomes: Collection<String> get() = dados.toList()
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: List<String> = listOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println(nomes)
+    println("Tem o nome Alex? ${nomes.contains("Alex")}")
+    println("Tamanho da coleção ${nomes.size}")
+}
